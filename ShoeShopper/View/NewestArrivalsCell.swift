d5b8e466e4c2c52d51e6970a94da6b67913f8d95
adiran <avg5074@gmail.com>
2020-07-20 10:42:42 -0400

    add collection view for newest arrival products, add collection view cell custom file

---
+++ ShoeShopper/View/NewestArrivalsCell.swift
@@ -0,0 +1,13 @@
+//
+//  NewestArrivalsCell.swift
+//  ShoeShopper
+//
+//  Created by Adrian Galecki on 7/20/20.
+//  Copyright © 2020 Adrian Galecki. All rights reserved.
+//
+
+import UIKit
+
+class NewestArrivalsCell: UICollectionViewCell {
+    
+}
